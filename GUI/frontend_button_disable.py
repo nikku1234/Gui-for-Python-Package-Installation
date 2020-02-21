4b993d15dc322164bc27e01d5feb20fd84c5d2e1
NIKHIL RAMESH <nikhilramesh1234@gmail.com>
2020-02-21 11:36:33 +0530

    Create frontend_button_disable.py

---
+++ GUI/frontend_button_disable.py
@@ -0,0 +1,27 @@
+from kivy.uix.button import Button
+ 
+from kivy.app import App
+ 
+from functools import partial
+ 
+class KivyButton(App):
+ 
+    def disable(self, instance, *args):
+ 
+        instance.disabled = True
+ 
+    def update(self, instance, *args):
+ 
+        instance.text = "I am Disabled!"
+ 
+    def build(self):
+ 
+        mybtn = Button(text="Click me to disable")
+ 
+        mybtn.bind(on_press=partial(self.disable, mybtn))
+ 
+        mybtn.bind(on_press=partial(self.update, mybtn))
+ 
+        return mybtn
+ 
+KivyButton().run()
